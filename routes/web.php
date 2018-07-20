c2733dfb1ab91b0ff3d7fc91f74b54d15f54b825
Lasha Palelashvili <falelashvili@gmail.com>
2018-07-20 19:10:33 +0400

    Admin table seeder

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('/{provider}/auth', 'SocialsController@auth')->name('social.auth');
 Route::get('/{provider}/redirect', 'SocialsController@callback')->name('social.callback');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('channels', 'ChannelsController');
+});
